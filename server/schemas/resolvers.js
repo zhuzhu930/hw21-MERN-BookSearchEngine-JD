06ba865a2eeb832e1c0bed5a47a4666f61c09996
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-24 21:46:50 -0500

    updating auth.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,28 +1,91 @@
-const { Tech, Matchup } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    users: async () => {
+      return User.find().populate('books');
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('books');
+    },
+    books: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    book: async (parent, { bookId }) => {
+      return Book.findOne({ _id: bookId });
     },
   },
+
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addUser: async (parent, { username, email, password }) => {
+      // First we create the user
+      const user = await User.create({ username, email, password });
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
+    login: async (parent, { email, password }) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+
+    addBook: async (parent, { authors, description, title, image, link }) => {
+      const book = await Book.create({ authors, description, title, image, link });
+
+      await User.findOneAndUpdate(
+        // ?Maybe shouldn't use username: 
+        { username: user },
+        { $addToSet: { books: book._id } }
       );
-      return vote;
+
+      return book;
     },
+
+    // addComment: async (parent, { thoughtId, commentText, commentAuthor }) => {
+    //   return Thought.findOneAndUpdate(
+    //     { _id: thoughtId },
+    //     {
+    //       $addToSet: { comments: { commentText, commentAuthor } },
+    //     },
+    //     {
+    //       new: true,
+    //       runValidators: true,
+    //     }
+    //   );
+    // },
+    removeBook: async (parent, { bookId }) => {
+      return Book.findOneAndDelete({ _id: bookId });
+    },
+    // removeComment: async (parent, { thoughtId, commentId }) => {
+    //   return Thought.findOneAndUpdate(
+    //     { _id: thoughtId },
+    //     { $pull: { comments: { _id: commentId } } },
+    //     { new: true }
+    //   );
+    // },
   },
 };
 
