6e68f64cc0084e4a359fe503805f4401ca8fe5b3
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-24 20:51:24 -0500

    npm install and refactor server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+// Add ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
 const path = require('path');
+// Add typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
-const routes = require('./routes');
+// Comment out the routes
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Adding ApolloServer
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,28 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Comment out the routes part
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Adding the homeroute and send the index.html from the build folder: 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
