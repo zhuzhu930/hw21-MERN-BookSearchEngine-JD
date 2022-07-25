2184fc8c1221c492513bab6e7f90c015ee1e7ac8
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-24 22:10:40 -0500

    adding queries and mutations to the client folder, modifying queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,40 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query me($username: String!) {
+    me(username: $username) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks
+    }
+  }
+`;
+
+// export const QUERY_THOUGHTS = gql`
+//   query getThoughts {
+//     thoughts {
+//       _id
+//       thoughtText
+//       thoughtAuthor
+//       createdAt
+//     }
+//   }
+// `;
+
+// export const QUERY_SINGLE_THOUGHT = gql`
+//   query getSingleThought($thoughtId: ID!) {
+//     thought(thoughtId: $thoughtId) {
+//       _id
+//       thoughtText
+//       thoughtAuthor
+//       createdAt
+//       comments {
+//         _id
+//         commentText
+//         createdAt
+//       }
+//     }
+//   }
+// `;
