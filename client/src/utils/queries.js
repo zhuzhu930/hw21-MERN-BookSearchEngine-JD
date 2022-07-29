5b21b1ccb7ef4979d5bc71965d2d8e2d3f1f20f3
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-28 22:33:35 -0500

    modifying queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,40 +1,20 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  query me($username: String!) {
-    me(username: $username) {
+  {
+    me {
       _id
       username
       email
       bookCount
-      savedBooks
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
-
-// export const QUERY_THOUGHTS = gql`
-//   query getThoughts {
-//     thoughts {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//     }
-//   }
-// `;
-
-// export const QUERY_SINGLE_THOUGHT = gql`
-//   query getSingleThought($thoughtId: ID!) {
-//     thought(thoughtId: $thoughtId) {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//       comments {
-//         _id
-//         commentText
-//         createdAt
-//       }
-//     }
-//   }
-// `;
